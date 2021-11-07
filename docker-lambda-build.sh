5cd8b1ab0c6ba228ec1b8030ceae4cba9033593f
Scott Donnelly <scott@donnel.ly>
2021-11-07 00:34:07 +0000

    fix: docker build was not actually writing to the hosts mounted volume. renamed bootstrap.zip to lambda.zip

--- docker-lambda-build.sh
+++ docker-lambda-build.sh
@@ -6,10 +6,10 @@ docker run --rm \
   -v "$PWD":/home/rust/src \
   -v cargo-git:/home/rust/.cargo/git \
   -v cargo-registry:/home/rust/.cargo/registry \
-  -v target:/home/rust/src/target \
+  -v target:/home/rust/target \
   ghcr.io/caddijp/rust-musl-builder/rust-musl-builder:1.55.0 \
   cargo build --release
 
 cp target/x86_64-unknown-linux-musl/release/bootstrap ./bootstrap
-rm bootstrap.zip || true
-zip -r9 -j bootstrap.zip bootstrap
+rm lambda.zip || true
+zip -r9 -j lambda.zip bootstrap
